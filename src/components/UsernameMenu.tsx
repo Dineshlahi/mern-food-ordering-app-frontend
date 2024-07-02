c0f45880ddedf80f647d7b0f64e76b599f92b865
SENEVIRATHNA S.M.D.L.L <85577080+Dineshlahi@users.noreply.github.com>
2024-07-02 20:05:37 +0530

    work with restaurant form

--- src/components/UsernameMenu.tsx
+++ src/components/UsernameMenu.tsx
@@ -1,6 +1,11 @@
 import { useAuth0 } from "@auth0/auth0-react";
 import { CircleUserRound } from "lucide-react";
-import { DropdownMenuContent, DropdownMenuItem, DropdownMenu, DropdownMenuTrigger } from "./ui/dropdown-menu";
+import {
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenu,
+  DropdownMenuTrigger,
+} from "./ui/dropdown-menu";
 import { Link } from "react-router-dom";
 import { Button } from "./ui/button";
 import { Separator } from "@radix-ui/react-separator";
@@ -15,18 +20,31 @@ const UsernameMenu = () => {
       </DropdownMenuTrigger>
       <DropdownMenuContent>
         <DropdownMenuItem>
+          <Link
+            to="/manage-restaurant"
+            className="font-bold hover:text-orange-500"
+          >
+            MAnage Restuarant
+          </Link>
+        </DropdownMenuItem>
+        <DropdownMenuItem>
           <Link to="/user-profile" className="font-bold hover:text-orange-500">
             User Profile
           </Link>
         </DropdownMenuItem>
         <Separator />
         <DropdownMenuItem>
-          <Button onClick={() => logout()} className="flex flex-1 font-bold bg-orange-500"> Log out</Button>
+          <Button
+            onClick={() => logout()}
+            className="flex flex-1 font-bold bg-orange-500"
+          >
+            {" "}
+            Log out
+          </Button>
         </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
-  )
-
-}
+  );
+};
 
 export default UsernameMenu;
---
+++ src/pages/ManageRestaurantPage.tsx
@@ -0,0 +1,7 @@
+import ManageRestaurantForm from "@/forms/user-profile-form/manage-restaurant-form/MangeRestaurantForm";
+
+const ManageRestaurantPage = () => {
+  return <ManageRestaurantForm />;
+};
+
+export default ManageRestaurantPage;
