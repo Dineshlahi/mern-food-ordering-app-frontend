cce9125bc74f5e9123fc9821ef01550f0a2f9a4d
SENEVIRATHNA S.M.D.L.L <85577080+Dineshlahi@users.noreply.github.com>
2024-07-26 10:33:57 +0530

    modify

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -20,6 +20,7 @@ const HomePage = () => {
         <SearchBar
           placeHolder="Search by city or Town"
           onSubmit={handleSearchSubmit}
+          searchQuery=""
         />
       </div>
       ``
